168a682d7e0c754dea09d5425e423d6fcd7be2fe
Marta Komar <markom108@op.pl>
2026-08-05 14:14:33 +0200

    dark theme draft

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import Topbar from "./components/topbar/Topbar";
+import { useState } from "react";
 
 function App(){
+  const[theme,setTheme] =useState("dark");
   return(
     <>
       <Topbar />
